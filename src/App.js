ea9154e952cdc89549cb815d1d86beca49c3e159
mjamalz <66275014+mjamalz@users.noreply.github.com>
2020-06-04 19:45:11 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to REACT WORLD!!</h1>
+        <h1>Welcome to REACT WORLD.. People!!!</h1>
       </header>
     </div>
   );
